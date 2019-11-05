6ff1fb97745dceeee8d867a933a7fc12e9cb9b6b
Tony Givargis <tony.givargis@gmail.com>
2019-11-05 09:20:08 -0800

    added tensorflow reference

---
+++ mnist/README.md
@@ -0,0 +1,31 @@
+# MNIST Benchmark
+
+This directory used the Gravity compiler to build a handwritten digits
+recognizer from the MNIST database [http://yann.lecun.com/exdb/mnist/].
+The implementation uses Gravity as a just-in-time compiler. A python3
+reference implementation is provided using tensorflow using exactly the
+same parameters.
+
+## Obtaining, Building & Running Gravity/MNIST
+  1. $ git clone https://github.com/givargis/gravity
+  2. $ cd gravity/src
+  3. $ make
+  4. $ cd ../mnist
+  5. $ make
+  6. $ ./mnist
+  7. $ python3 mnist.py
+
+## Performance
+
+All benchmarks running on:
+  * CentOS Linux release 8.0.1905 (Core)
+  * 4 G Hz Quad-Core Intel Core i7
+  * 32 GB 1867 MHz DDR3
+  * Train/Test data preloaded in RAM and excluded from time measurements
+
+   Implementation  | Accuracy |  Train Time   |   Test Time
+                   |          | (usec/sample) | (usec/sample)
+-------------------|----------|---------------|---------------
+     Gravity/C     |  0.9670  |     124       |      18
+-------------------|--------------------------|---------------
+ Python/Tensorflow |  0.9748  |     183       |      39
---
+++ mnist/mnist.py
@@ -0,0 +1,38 @@
+#
+# mnist.c
+# Copyright (C) Tony Givargis, 2019
+#
+# This file is part of The Gravity Compiler.
+#
+# The Gravity Compiler is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version. The Gravity Compiler is distributed in
+# the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details. You should
+# have received a copy of the GNU General Public License along with Foobar.
+# If not, see <https://www.gnu.org/licenses/>.
+#
+
+import tensorflow as tf
+
+(train_x, train_y), (test_x, test_y) = tf.keras.datasets.mnist.load_data()
+
+train_x = tf.keras.utils.normalize(train_x, axis=1)
+test_x = tf.keras.utils.normalize(test_x, axis=1)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(100, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(100, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_x, train_y, batch_size=8, epochs=4)
+
+val_loss, val_acc = model.evaluate(test_x, test_y)
+print("Accuracy: ", val_acc)
